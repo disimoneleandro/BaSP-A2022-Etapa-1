87729b642af03cf76574f6cb66080019bb14ab6a
Leandro Di Simone <disimoneleandro@gmail.com>
2022-09-27 18:49:49 -0300

    BaSP-W06-2022: finished sing up javascript

--- Week-06/javascript/sing-up.js
+++ Week-06/javascript/sing-up.js
@@ -19,8 +19,6 @@ var inpSingUpError = document.createElement("p");
 inpSingUpError.classList.add("text-error");
 
 
-// Validate Name
-
 inpName.onblur = function() {
     if(inpName.value.trim() == ""){
         inpName.classList.add("red-border");
@@ -34,8 +32,7 @@ inpName.onblur = function() {
         inpName.classList.add("red-border");
         inpSingUpError.innerHTML = "*Requery only letters";
         inpName.insertAdjacentElement("afterend", inpSingUpError);
-    }else{ inpName.classList.add("green-border");
-}
+    }else{ inpName.classList.add("green-border");}
 }
 
 inpName.onfocus = function(){
@@ -43,10 +40,6 @@ inpName.onfocus = function(){
     inpSingUpError.remove();
 }
 
-
-
-
-// Validate LastName
 
 inpLastName.onblur = function() {
     if(inpLastName.value.trim() == ""){
@@ -61,8 +54,7 @@ inpLastName.onblur = function() {
         inpLastName.classList.add("red-border");
         inpSingUpError.innerHTML = "*Requery only letters";
         inpLastName.insertAdjacentElement("afterend", inpSingUpError);
-    }else{ inpLastName.classList.add("green-border");
-}
+    }else{ inpLastName.classList.add("green-border");}
 }
 
 inpLastName.onfocus = function(){
@@ -70,8 +62,6 @@ inpLastName.onfocus = function(){
     inpSingUpError.remove();
 }
 
-
-// Validate ID
 
 inpId.onblur = function(){
     if(inpId.value.trim() ==""){
@@ -86,7 +76,7 @@ inpId.onblur = function(){
         inpId.classList.add("red-border");
         inpSingUpError.innerHTML = "*Requery only numbers";
         inpId.insertAdjacentElement("afterend", inpSingUpError);
-    }else{ inpId.classList.add("green-border"); }
+    }else{ inpId.classList.add("green-border");}
 }
 
 inpId.onfocus = function(){
@@ -94,7 +84,6 @@ inpId.onfocus = function(){
     inpSingUpError.remove();
 }
 
-// Validate Phone Number
 
 inpPhoneNum.onblur = function(){
     if(inpPhoneNum.value == ""){
@@ -109,7 +98,7 @@ inpPhoneNum.onblur = function(){
         inpPhoneNum.classList.add("red-border");
         inpSingUpError.innerHTML = "*Requery only numbers";
         inpPhoneNum.insertAdjacentElement("afterend", inpSingUpError);
-    }else{ inpPhoneNum.classList.add("green-border"); }
+    }else{ inpPhoneNum.classList.add("green-border");}
 }
 
 inpPhoneNum.onfocus = function(){
@@ -118,19 +107,28 @@ inpPhoneNum.onfocus = function(){
 }
 
 
+inpAdress.onblur = function(){
+    if(inpAdress.value == ""){
+        inpAdress.classList.add("red-border");
+        inpSingUpError.innerHTML = "*Empty adress";
+        inpAdress.insertAdjacentElement("afterend", inpSingUpError);
+    }else if ( inpAdress.value.length < 5 ){
+        inpAdress.classList.add("red-border");
+        inpSingUpError.innerHTML = "Adress must be at least 5 characters";
+        inpAdress.insertAdjacentElement("afterend", inpSingUpError);
+    }else if ( !hasNumbersAndCharAndSpaces(inpAdress.value) ){
+        inpAdress.classList.add("red-border");
+        inpSingUpError.innerHTML = "Only numbers,letters and spaces";
+        inpAdress.insertAdjacentElement("afterend", inpSingUpError);
+    }
+    else{ inpAdress.classList.add("green-border");}
+}
 
+inpAdress.onfocus = function(){
+    inpAdress.classList.remove("red-border");
+    inpSingUpError.remove();
+}
 
-
-// Validate Adress
-
-
-
-
-
-
-
-
-// Validate City
 
 inpCity.onblur = function(){
     if(inpCity.value.length == ""){
@@ -141,15 +139,11 @@ inpCity.onblur = function(){
         inpCity.classList.add("red-border");
         inpSingUpError.innerHTML = "*Require min 4 characters";
         inpCity.insertAdjacentElement("afterend", inpSingUpError);
-    }else if( isChars(inpCity.value) ){
+    }else if( !hasNumbersAndChar(inpCity.value) ){
         inpCity.classList.add("red-border");
-        inpSingUpError.innerHTML = "*Requery min 4 letters";
+        inpSingUpError.innerHTML = "*Only characters and numbers";
         inpCity.insertAdjacentElement("afterend", inpSingUpError);
-    // }else if( !hasNumbersAndChar(inpCity.value) ){
-    //     inpCity.classList.add("red-border");
-    //     inpSingUpError.innerHTML = "*Only characters and numbers";
-    //     inpCity.insertAdjacentElement("afterend", inpSingUpError);
-    }else{ inpCity.classList.add("green-border"); }
+    }else{ inpCity.classList.add("green-border");}
 }
 
 inpCity.onfocus = function(){
@@ -158,7 +152,6 @@ inpCity.onfocus = function(){
 }
 
 
-// Validate Adress City
 
 inpAdressCode.onblur = function(){
     if(inpAdressCode.value == ""){
@@ -173,7 +166,7 @@ inpAdressCode.onblur = function(){
         inpAdressCode.classList.add("red-border");
         inpSingUpError.innerHTML = "*Requery only numbers";
         inpAdressCode.insertAdjacentElement("afterend", inpSingUpError);
-    }else{ inpAdressCode.classList.add("green-border"); }
+    }else{ inpAdressCode.classList.add("green-border");}
 }
 
 inpAdressCode.onfocus = function(){
@@ -181,8 +174,6 @@ inpAdressCode.onfocus = function(){
     inpSingUpError.remove();
 }
 
-
-// Validate Email
 
 inpEmail.onblur = function() {
     if (inpEmail.value == ""){
@@ -194,9 +185,7 @@ inpEmail.onblur = function() {
         inpEmail.classList.add("red-border");
         inpSingUpError.innerHTML = "Please insert email valid";
         inpEmail.insertAdjacentElement("afterend", inpSingUpError);
-    } else{ inpEmail.classList.add("green-border");
-
-    }
+    } else{ inpEmail.classList.add("green-border");}
 }
 
 inpEmail.onfocus = function(){
@@ -205,7 +194,6 @@ inpEmail.onfocus = function(){
 }
 
 
-// Validate password
 
 inpPassword.onblur = function() {
     if (inpPassword.value == ""){
@@ -218,12 +206,10 @@ inpPassword.onblur = function() {
         inpPassword.insertAdjacentElement("afterend", inpSingUpError);
     }else if ( !hasNumbersAndChar(inpPassword.value)){
         inpPassword.classList.add("red-border");
-        inpSingUpError.innerHTML = "Only numbers and characters";
+        inpSingUpError.innerHTML = "Only numbers and letters";
         inpPassword.insertAdjacentElement("afterend", inpSingUpError);
     }
-    else{ inpPassword.classList.add("green-border");
-    }
-
+    else{ inpPassword.classList.add("green-border");}
 }
 
 inpPassword.onfocus = function(){
@@ -232,7 +218,6 @@ inpPassword.onfocus = function(){
 }
 
 
-// Validate Confirm Password
 
 inpRepPassword.onblur = function() {
     if (inpRepPassword.value == ""){
@@ -245,12 +230,10 @@ inpRepPassword.onblur = function() {
         inpRepPassword.insertAdjacentElement("afterend", inpSingUpError);
     }else if ( !hasNumbersAndChar(inpRepPassword.value)){
         inpRepPassword.classList.add("red-border");
-        inpSingUpError.innerHTML = "Only numbers and characters";
+        inpSingUpError.innerHTML = "Only numbers and letters";
         inpRepPassword.insertAdjacentElement("afterend", inpSingUpError);
     }
-    else{ inpRepPassword.classList.add("green-border");
-    }
-
+    else{ inpRepPassword.classList.add("green-border");}
 }
 
 inpRepPassword.onfocus = function(){
@@ -259,9 +242,82 @@ inpRepPassword.onfocus = function(){
 }
 
 
+btnSingUp.onclick = function(e){
+    e.preventDefault();
+    if(inpName.value == ""){
+        alert("Please insert your name ");
+    }else if(inpLastName.value == ""){
+        alert("Please insert your last name");
+    }else if(inpId.value == ""){
+        alert("Please insert your ID");
+    }else if(inpDateBrth.value == ""){
+        alert("Please insert your date birth");
+    }else if(inpPhoneNum.value == ""){
+        alert("Please insert your phone number");
+    }else if(inpAdress.value == ""){
+        alert("Please insert your adress");
+    }else if(inpCity.value == ""){
+        alert("Please insert your city");
+    }else if(inpAdressCode.value == ""){
+        alert("Please insert your adress code");
+    }else if(inpEmail.value == ""){
+        alert("Please insert your email");
+    }else if(inpPassword.value == ""){
+        alert("Please insert your password");
+    }else if(inpRepPassword.value == ""){
+        alert("Please insert repeat password");
+    }else if (inpName.value.trim() == ""){
+        alert("Name is empty");
+    }else if (inpName.value.length <= 3 ){
+        alert("Name requery min 4 characters");
+    }else if (!hasCharsAndSpaces(inpName.value)){
+        alert("Name requery only letters");
+    }else if(inpLastName.value.length <= 3){
+        alert("Last name requery min 4 characters");
+    }else if(!hasCharsAndSpaces(inpLastName.value)){
+        alert("Last name requery only letters");
+    }else if(inpId.value.length < 7){
+        alert("Id requery min 8 numbers");
+    }else if(!onlyNumbers(inpId.value)){
+        alert("Id requery only numbers");
+    }else if(inpPhoneNum.value.length <= 9 || inpPhoneNum.value.length >= 11 ){
+        alert("Phone number requery only 10 numbers");
+    }else if(!onlyNumbers(inpPhoneNum.value)){
+        alert("Phone number requery only numbers");
+    }else if(inpAdress.value.length < 5 ){
+        alert("Adress must be at least 5 characters");
+    } else if(!hasNumbersAndChar(inpAdress.value)){
+        alert("Only numbers and letters");
+    }else if(inpCity.value.length <= 3){
+        alert("City requery min 4 characters");
+    }else if(!hasNumbersAndChar(inpCity.value)){
+        alert("City requery only numbers and letters");
+    }else if(inpAdressCode.value.length < 4 || inpAdressCode.value.length > 5 ){
+        alert("Adrees code requery between 4 and 5 numbers");
+    }else if(!onlyNumbers(inpAdressCode.value)){
+        alert("Adress code requery only numbers");
+    }else if(!emailExpression.test(inpEmail.value)){
+        alert("Please insert Email valid");
+    }else if(inpPassword.value.length < 8 || inpPassword.value.length > 20){
+        alert("Password needs 8 to 20 characters");
+    }else if(!hasNumbersAndChar(inpPassword.value)){
+        alert("Password requery only numbers and letters");
+    }else if(inpRepPassword.value.length < 8 || inpRepPassword.value.length > 20){
+        alert("Repeat password requery only numbers and letters");
+    }else if(!hasNumbersAndChar(inpRepPassword.value)){
+        alert("Repeat password requery only numbers and letters");
+    }else{
+        alert("Name: " + inpName.value + "\n" + "Last Name : " + inpLastName.value + "\n" + "ID: " + inpId.value + "\n" +
+              "Phone number: " + inpPhoneNum.value + "\n" + "Adress : " + inpAdress.value + "\n" + "City : " + inpCity.value + "\n" + "Adress Code : " + "\n" +
+              "Email : " + inpEmail.value + "\n" + "Password : " + inpPassword.value + "\n" + "Repeat Pass: " + inpRepPassword.value);}
+}
+
+btnBack.onclick = function(e){
+    window.location.href="./index.html";
+}
 
 
-// Functions to Help
+
 
 function hasCharsAndSpaces(parameter) {
     for (var i = 0 ; i < parameter.length; i++) {
@@ -282,6 +338,16 @@ function isNumber(parameter) {
     return false;
 }
 
+function isSpaces(parameter) {
+    var spaces = " ";
+    for (var i = 0; i < parameter.length; i++) {
+      if (spaces.includes(parameter[i])) {
+        return true;
+      }
+    }
+    return false;
+}
+
 function onlyNumbers(parameter) {
     for (var i = 0; i < parameter.length; i++) {
       if (!isNumber(parameter[i])) {
@@ -289,7 +355,8 @@ function onlyNumbers(parameter) {
       }
     }
     return true;
-  }
+}
+
 
 function isChars(parameter) {
     for (var i = 0 ; i < parameter.length; i++) {
@@ -301,15 +368,13 @@ function isChars(parameter) {
 }
 
 // function minFourthChars(parameter) {
-//     for (var i = 0 ; i < parameter.length; i++) {
-//       if (((parameter[i] >= 'a' && parameter[i] <= 'z') || (parameter[i] >= 'A' && parameter[i] <= 'Z')).length > 4) {
+
+//     if ( hasCharacters(parameter) && parameter.length > 4 ) {
 //         return true;
 //       }
-//     }
 //     return false;
 // }
-
-// console.log(minFourthChars("leandro"))
+// console.log(minFourthChars("prueba1"))
 
 
 function hasNumbersAndChar(parameter) {
@@ -329,7 +394,25 @@ function hasNumbersAndChar(parameter) {
     return num && char;
 }
 
+function hasNumbersAndCharAndSpaces(parameter) {
+    var num = false;
+    var char = false;
+    var spaces = false;
 
+    for (var i = 0; i < parameter.length; i++) {
+      if (isNumber(parameter[i])) {
+        num = true;
+      } else if (isChars(parameter[i])) {
+        char = true;
+      } else if( isSpaces(parameter[i])) {
+        spaces = true;
+      }else {
+        return false;
+      }
+    }
+
+    return num && char;
+}
 
 
 
